0391646a125fdf513ee839f2eb90de6d1771a864
Shahzod <87805232+shahzodmaxammatov288@users.noreply.github.com>
2021-10-08 21:00:22 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+window.addEventListener("DOMContentLoaded", () => {
+	const loader = document.querySelector(".loader");
+	setTimeout(function () {
+		loader.style.opacity = "0";
+		setTimeout(function () {
+			loader.style.display = "none";
+		}, 250);
+	}, 500);
+});
+
